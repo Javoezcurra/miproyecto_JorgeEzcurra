23e929a4f436d2702ceb0e1cb97c7813c6c89a7b
Jorge Ezcurra <javodefractus@gmail.com>
2021-10-17 19:44:55 -0300

    Agregue el Navbar y modifique un poco el css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
+import Navbar from "./Components/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Sea bienvenido a la primera prueba</h1>
+        <h1 className="title">Nerdoverse</h1>
+        <h2>Tu lugar de compras Geeks!</h2>
+        
+
+        <Navbar />
+
         <img src={logo} className="App-logo" alt="logo" />
         
         <p>
-          Segunda <code>clase</code> de React en Coder.
+          Segundo <code>desafío</code> de React en Coder.
         </p>
+
+        <h3>La web va tomando forma</h3>
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ my-app/src/Components/Navbar.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+
+function Lmenu() {
+
+    return (
+ <div>
+ <main>
+   <ul className="Main">
+     <li className="menu">Noticias</li>
+     <li className="menu">Reseñas</li>
+     <li className="menu">Productos</li>
+     <li className="menu">Contacto</li>
+   </ul>
+ </main>
+</div>
+    )
+
+}
+
+export default Lmenu;
